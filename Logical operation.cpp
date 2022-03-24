5610be0c45930f469fbbb319a0b73b19a7466355
Heo-jaehyeon <gjwogus01@naver.com>
2022-03-24 11:43:05 +0000

    비트 연산자

--- Logical operation.cpp
+++ Logical operation.cpp
@@ -1,28 +1,31 @@
 #include <iostream>
 using namespace std;
 
-// 비교 연산
+// 논리 연산
 
 bool isSame;
-bool isDifferent;
-bool isGreater;
-bool isSmaller;
+bool test;
+
+int a = 1;
+int b = 1;
+
+int hp = 100;
+bool isInvincible = true;
+
+isSame = (a == b);
 
 int main()
 {
-	// a == b : a와 b의 값이 같은가?
-	// 같으면 1, 다르면 0
-	isSame = (a == b);
+	test = !isSame;
+	// ! 의 뜻 not
+	// 0이면 1, 그 외 0
+	// test 값 0
 	
-	// a != b : a와 b의 값이 다른가?
-	isDifferent = (a != b);
+	test = (hp <= 0 && isInvincible == false); // 죽음!
+	// && 의 뜻 AND
+	// a && b -> 둘 다 1이면 1, 다르면 0
 	
-	// a > b : a가 b보다 큰가?
-	// a >= b : a가 b보다 같거나 큰가?
-	isGreater = (a > b);
-	
-	// a < b : a가 b보다 작은가?
-	// a <= b : a가 b보다 작거나 같은가?
-	isSmaller = (a < b);
-	
+	test = (hp > 0 || isInvincible == true); // 살았음!
+	// || 의 뜻 OR
+	// a || b -> 둘 중 하나라도 1이면 1 (둘 다 0이면 0)
 }
